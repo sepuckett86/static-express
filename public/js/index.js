3d47774e88944f501482c5a669a2cdccd03bc6a6
sepuckett86 <sepuckett86@gmail.com>
2019-07-10 16:48:18 -0700

    add home page list of colors

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+const main = document.getElementById('root');
+const ul = document.createElement('ul');
+
+fetch('/api/v1/colors')
+  .then(res => res.json())
+  .then(colors => {
+    colors.forEach(color => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = `/colors?${color.name}`;
+      a.textContent = color.name;
+      li.appendChild(a);
+      ul.appendChild(li);
+    });
+  });
+
+main.appendChild(ul);
