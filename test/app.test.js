5af2e147fbe86ab858bdcc58284e353d03a39beb
sepuckett86 <sepuckett86@gmail.com>
2019-07-10 16:04:16 -0700

    create first test

--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,25 @@
+const app = require('../lib/app');
+const request = require('supertest');
+
+describe('app', () => {
+  /*
+  create a /api/v1/colors route. 
+  It should return an array of colors 
+  [{ name: red, hex: 'FF0000', r: 255, g: 0, b: 0 }...]
+  */
+  it('returns a color array for /api/v1/colors route', () => {
+    return request(app)
+      .get('/api/v1/colors')
+      .then(res => {
+        expect(res.body)
+          .toEqual(expect.arrayContaining([{ 
+            name: 'red', 
+            hex: 'FF0000', 
+            r: 255, 
+            g: 0, 
+            b: 0 
+          }]));
+      });
+  });
+});
 
