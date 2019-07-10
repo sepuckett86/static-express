8831408bf079d94410d5981c20c32365de83cc94
sepuckett86 <sepuckett86@gmail.com>
2019-07-10 16:20:47 -0700

    add passing test

--- test/app.test.js
+++ test/app.test.js
@@ -21,5 +21,24 @@ describe('app', () => {
           }]));
       });
   });
+
+  /*
+  create a /api/v1/colors/:name route. 
+  It should return the color from your 
+  array of colors by that name.
+  */
+  it('returns a specific color with "/api/v1/colors/:name"', () => {
+    return request(app)
+      .get('/api/v1/colors/red')
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'red', 
+          hex: 'FF0000', 
+          r: 255, 
+          g: 0, 
+          b: 0 
+        });
+      });
+  });
 });
 
